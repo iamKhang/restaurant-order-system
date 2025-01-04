4e77d22deb04154bb288e61bc5a678fad0ca474d
iamKhangg <iamhoangkhang@icloud.com>
2025-01-04 23:12:01 +0700

    Khởi tạo table trong cơ sở dữ liệu với Prisma (Demo)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
-import { Module } from '@nestjs/common';
+import { SupabaseService } from './services/supabase.service';
+import { OrdersGateway } from './gateways/orders.gateway';
+import { OrdersService } from './services/order.service';
+import { OrdersController } from './controllers/orders.controller';
 import { AppController } from './app.controller';
+import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  providers: [SupabaseService, OrdersGateway, OrdersService, AppService],
+  exports: [SupabaseService],
+  controllers: [AppController, OrdersController],
 })
 
 export class AppModule {}
